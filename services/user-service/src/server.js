f3a1e51ed352effa409c81ec7f2adb45bf228233
JUEL <juelrana775581@gmail.com>
2026-06-19 14:17:30 +0600

    feat(user-service): initialize user service with Express and MySQL connection
    - Added package.json for user service with necessary dependencies.
    - Created app.js to set up Express application and middleware.
    - Implemented db.config.js to configure MySQL connection pool.
    - Developed server.js to start the server and handle database connection.

---
+++ services/user-service/src/server.js
@@ -0,0 +1,23 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+import DB from './config/db.config.js';
+dotenv.config({ quiet: true });
+
+
+DB.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        process.exit(1); // Exit the application if the database connection fails
+    } else {
+        console.log(`✅ Database connected successfully with ${process.env.DB_NAME} database`);
+        connection.release();
+
+
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => {
+            console.log(`✅ User service is running on port ${PORT}`);
+            console.log("http://localhost:" + PORT + "/");
+        });
+    }
+});
+
